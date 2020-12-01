60570af86021b9ee508bb63f9c33bacf639e04d6
shivanisriup <47216897+shivanisriup@users.noreply.github.com>
2020-12-01 11:54:41 +0530

    mall_customer_segmentation_dotnet_project

---
+++ MLProject/ClusteringProject/ClusteringProject/MallData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace ClusteringProject
+{
+    public class MallData
+    {
+        [LoadColumn(3)]
+        public float AnnualIncome;
+
+        [LoadColumn(4)]
+        public float SpendingScore;
+
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
